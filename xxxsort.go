43cb9c35480828d005448d3995c2bb5df6bc1244
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2024-01-11 22:43:10 +0700

    Add quicksort

--- xxxsort.go
+++ xxxsort.go
@@ -118,3 +118,51 @@ func InsertionSort[T cmp.Ordered](s []T, mode string) ([]T, error) {
 		return nil, fmt.Errorf("%v not available, use instead 'asc' or 'desc'", mode)
 	}
 }
+
+func partitionAsc[T cmp.Ordered](s []T, low int, high int) int {
+	pivot := s[high]
+	i := low - 1
+
+	for j := low; j <= high; j++ {
+		if s[j] < pivot {
+			i++
+			s[i], s[j] = s[j], s[i]
+		}
+	}
+
+	s[i+1], s[high] = s[high], s[i+1]
+
+	return i + 1
+}
+
+func partitionDesc[T cmp.Ordered](s []T, low int, high int) int {
+	pivot := s[high]
+	i := low - 1
+
+	for j := low; j <= high; j++ {
+		if s[j] > pivot {
+			i++
+			s[i], s[j] = s[j], s[i]
+		}
+	}
+
+	s[i+1], s[high] = s[high], s[i+1]
+
+	return i + 1
+}
+
+func QuickSortAsc[T cmp.Ordered](s []T, low int, high int) {
+	if low < high {
+		part := partitionAsc(s, low, high)
+		QuickSortAsc(s, low, part-1)
+		QuickSortAsc(s, part+1, high)
+	}
+}
+
+func QuickSortDesc[T cmp.Ordered](s []T, low int, high int) {
+	if low < high {
+		part := partitionDesc(s, low, high)
+		QuickSortDesc(s, low, part-1)
+		QuickSortDesc(s, part+1, high)
+	}
+}
