63b4e5df576f761e1e8fd53a14afffaf05af84db
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2024-01-20 16:15:31 +0700

    update func

--- xxxsort.go
+++ xxxsort.go
@@ -2,120 +2,101 @@ package xxxsort
 
 import (
 	"cmp"
-	"fmt"
 )
 
-func BubbelSort[T cmp.Ordered](s []T, mode string) ([]T, error) {
-	if mode == "asc" {
-		for i := 0; i < len(s); i++ {
-			is_swap := false
-			for j := 0; j < len(s)-i-1; j++ {
-				if s[j] > s[j+1] {
-					s[j], s[j+1] = s[j+1], s[j]
-					is_swap = true
-				}
+func BubbelSortAsc[T cmp.Ordered](s []T) {
+	for i := 0; i < len(s); i++ {
+		is_swap := false
+		for j := 0; j < len(s)-i-1; j++ {
+			if cmp.Compare(s[j], s[j+1]) == 1 {
+				s[j], s[j+1] = s[j+1], s[j]
+				is_swap = true
 			}
+		}
 
-			if !is_swap {
+		if !is_swap {
+			break
+		}
+	}
+}
+
+func BubbelSortDesc[T cmp.Ordered](s []T) {
+	for i := 0; i < len(s); i++ {
+		is_swap := false
+		for j := 0; j < len(s)-i-1; j++ {
+			if cmp.Compare(s[j], s[j+1]) == -1 {
+				s[j], s[j+1] = s[j+1], s[j]
+				is_swap = true
+			}
+		}
+
+		if !is_swap {
+			break
+		}
+	}
+}
+
+func SelectionSortAsc[T cmp.Ordered](s []T) {
+	for i := 0; i < len(s)-1; i++ {
+		lowestNumberIdx := i
+		for j := i + 1; j < len(s); j++ {
+			if cmp.Compare(s[j], s[lowestNumberIdx]) == -1 {
+				lowestNumberIdx = j
+			}
+		}
+
+		if lowestNumberIdx != i {
+			s[i], s[lowestNumberIdx] = s[lowestNumberIdx], s[i]
+		}
+	}
+}
+
+func SelectionSortDesc[T cmp.Ordered](s []T) {
+	for i := 0; i < len(s)-1; i++ {
+		lowestNumberIdx := i
+		for j := i + 1; j < len(s); j++ {
+			if cmp.Compare(s[j], s[lowestNumberIdx]) == 1 {
+				lowestNumberIdx = j
+			}
+		}
+
+		if lowestNumberIdx != i {
+			s[i], s[lowestNumberIdx] = s[lowestNumberIdx], s[i]
+		}
+	}
+}
+
+func InsertionSortAsc[T cmp.Ordered](s []T) {
+	for i := 1; i < len(s); i++ {
+		tempVal := s[i]
+		position := i - 1
+		for position >= 0 {
+			if cmp.Compare(tempVal, s[position]) == -1 {
+				s[position+1] = s[position]
+				position--
+			} else {
 				break
 			}
 		}
 
-		return s, nil
-	} else if mode == "desc" {
-		for i := 0; i < len(s); i++ {
-			is_swap := false
-			for j := 0; j < len(s)-i-1; j++ {
-				if s[j] < s[j+1] {
-					s[j], s[j+1] = s[j+1], s[j]
-					is_swap = true
-				}
-			}
+		s[position+1] = tempVal
+	}
+}
 
-			if !is_swap {
+func InsertionSortDesc[T cmp.Ordered](s []T) {
+	for i := 1; i < len(s); i++ {
+		tempVal := s[i]
+		position := i - 1
+		for position >= 0 {
+			if cmp.Compare(tempVal, s[position]) == 1 {
+				s[position+1] = s[position]
+				position--
+			} else {
 				break
 			}
 		}
 
-		return s, nil
-	} else {
-		return nil, fmt.Errorf("%v not available, use instead 'asc' or 'desc'", mode)
-	}
-}
-
-func SelectionSort[T cmp.Ordered](s []T, mode string) ([]T, error) {
-	if mode == "asc" {
-		for i := 0; i < len(s)-1; i++ {
-			lowestNumberIdx := i
-			for j := i + 1; j < len(s); j++ {
-				if s[j] < s[lowestNumberIdx] {
-					lowestNumberIdx = j
-				}
-			}
-
-			if lowestNumberIdx != i {
-				s[i], s[lowestNumberIdx] = s[lowestNumberIdx], s[i]
-			}
-		}
-
-		return s, nil
-	} else if mode == "desc" {
-		for i := 0; i < len(s)-1; i++ {
-			lowestNumberIdx := i
-			for j := i + 1; j < len(s); j++ {
-				if s[j] > s[lowestNumberIdx] {
-					lowestNumberIdx = j
-				}
-			}
-
-			if lowestNumberIdx != i {
-				s[i], s[lowestNumberIdx] = s[lowestNumberIdx], s[i]
-			}
-		}
-
-		return s, nil
-	} else {
-		return nil, fmt.Errorf("%v not available, use instead 'asc' or 'desc'", mode)
-	}
-}
-
-func InsertionSort[T cmp.Ordered](s []T, mode string) ([]T, error) {
-	if mode == "asc" {
-		for i := 1; i < len(s); i++ {
-			tempVal := s[i]
-			position := i - 1
-			for position >= 0 {
-				if tempVal < s[position] {
-					s[position+1] = s[position]
-					position--
-				} else {
-					break
-				}
-			}
-
-			s[position+1] = tempVal
-		}
-
-		return s, nil
-	} else if mode == "desc" {
-		for i := 1; i < len(s); i++ {
-			tempVal := s[i]
-			position := i - 1
-			for position >= 0 {
-				if tempVal > s[position] {
-					s[position+1] = s[position]
-					position--
-				} else {
-					break
-				}
-			}
-
-			s[position+1] = tempVal
-		}
-
-		return s, nil
-	} else {
-		return nil, fmt.Errorf("%v not available, use instead 'asc' or 'desc'", mode)
+		s[position+1] = tempVal
 	}
 }
 
@@ -124,23 +105,7 @@ func partitionAsc[T cmp.Ordered](s []T, low int, high int) int {
 	i := low - 1
 
 	for j := low; j <= high; j++ {
-		if s[j] < pivot {
-			i++
-			s[i], s[j] = s[j], s[i]
-		}
-	}
-
-	s[i+1], s[high] = s[high], s[i+1]
-
-	return i + 1
-}
-
-func partitionDesc[T cmp.Ordered](s []T, low int, high int) int {
-	pivot := s[high]
-	i := low - 1
-
-	for j := low; j <= high; j++ {
-		if s[j] > pivot {
+		if cmp.Compare(s[j], pivot) == -1 {
 			i++
 			s[i], s[j] = s[j], s[i]
 		}
@@ -157,6 +122,22 @@ func QuickSortAsc[T cmp.Ordered](s []T, low int, high int) {
 		QuickSortAsc(s, low, part-1)
 		QuickSortAsc(s, part+1, high)
 	}
+}
+
+func partitionDesc[T cmp.Ordered](s []T, low int, high int) int {
+	pivot := s[high]
+	i := low - 1
+
+	for j := low; j <= high; j++ {
+		if cmp.Compare(s[j], pivot) == 1 {
+			i++
+			s[i], s[j] = s[j], s[i]
+		}
+	}
+
+	s[i+1], s[high] = s[high], s[i+1]
+
+	return i + 1
 }
 
 func QuickSortDesc[T cmp.Ordered](s []T, low int, high int) {
